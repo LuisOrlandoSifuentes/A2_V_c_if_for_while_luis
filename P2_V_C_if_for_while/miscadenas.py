0ac38034daba17dbf5f2c7cb0e9d3e7618dfd764
LuisOrlandoSifuentes <a.22308051281313@cbtis128.edu.mx>
2024-09-06 09:35:27 -0600

    Add files via upload

---
+++ P2_V_C_if_for_while/miscadenas.py
@@ -0,0 +1,23 @@
+"""Luis Sifuentes
+trabajando con cadenas
+"""
+nombre="Luis"
+print(nombre[1])
+print(nombre)
+
+MisNovias=["L","U","I","S"]
+for chicas in MisNovias:
+    print(chicas)
+    
+    print("--otro ejemplo--")
+    b = "Luis Sifuentes"
+    print(b[2:5])
+    
+    print("-- a mayusculas--")
+    a= "luis Sifuentes"
+    print(a.upper())
+    print("--Concatenar--")
+a = "Maria"
+b = "Perez"
+c = a + "_" + b
+print(c)
